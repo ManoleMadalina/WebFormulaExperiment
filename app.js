a396e617eca6d4a3160179019a538a56401b5220
Manole Mădălina <67560247+ManoleMadalina@users.noreply.github.com>
2020-09-04 10:20:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -109,3 +109,18 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
+
+/*Animate On Load*/
+const images = document.querySelectorAll(".anim");
+observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim1 1.3s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = "none";
+    }
+  });
+});
+images.forEach((image) => {
+  observer.observe(image);
+});
